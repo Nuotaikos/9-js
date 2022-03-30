089e867610ad6dfb1b049748952957275846683f
Diana <oganauskiene@gmail.com>
2022-03-30 10:37:16 +0300

    Numbers

---
+++ js/01-variables.js
@@ -0,0 +1,34 @@
+const lucyNumber = 7;
+console.log('Laucky namber', lucyNumber);
+
+let pinigine = 0
+console.log('Pinigine', pinigine);
+
+pinigine = 5
+console.log(pinigine);
+
+pinigine = 7
+console.log(pinigine);
+
+const vardas = 'Maryte';
+const pasisveikinimas = 'Laba diena, ' + vardas + '.'
+console.log(pasisveikinimas);
+
+const arVedes = true;
+const arTuriVaiku = false;
+const arMeluojaDelVaiku = true;
+console.log('Ar vedes?', arVedes);
+console.log('Ar meluoja?', arTuriVaiku);
+
+console.log(2 + 2 * 2);
+console.log("2 + 2 * 2");
+
+console.log(true, false);
+console.log('true', 'false');
+
+const pazymiai =[10, 2, 8, 6];
+console.log('Pazymiai:', pazymiai);
+
+const studentai = ['Petras', 'JOnas', 'Maryte'];
+console.log(studentai);
+
