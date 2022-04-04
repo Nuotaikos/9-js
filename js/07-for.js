b5b177ae3a7cae60b1c05548dae2b3b365349130
Diana <oganauskiene@gmail.com>
2022-04-04 11:05:27 +0300

    arrow-functions.js

--- js/07-for.js
+++ js/07-for.js
@@ -1,16 +1,42 @@
 /*
-LOOP: for, while, for-in, for-of, foreach, ...
+LOOP: for, while, for-of, foreach, ...
+OBJECT-LOOP: for-in
 */
 
 const pazymiai = [10, 2, 8, 4, 6];
-let suma = 0;
 
-for (let i = 0; i < pazymiai.length; i++) {
-    const pazymys = pazymiai[i];
-    suma += pazymys;
-
-    console.log(`${i}) pazymys ${pazymys} [${suma}]`);
+// FOR
+let sumaFor = 0;
+for (let i1 = 0; i1 < pazymiai.length; i1++) {
+    const pazymys = pazymiai[i1];
+    sumaFor += pazymys;
+    console.log(`${i1}) pazymys ${pazymys} [${sumaFor}]`);
 }
 
-const vidurkis = suma / pazymiai.length;
-console.log('Vidurkis:', vidurkis);
+// WHILE
+console.log('-----------');
+let sumaWhile = 0;
+let i2 = 0;
+while (i2 < pazymiai.length) {
+    const pazymys = pazymiai[i2];
+    sumaWhile += pazymys;
+    console.log(`${i2}) pazymys ${pazymys} [${sumaWhile}]`);
+    i2++;
+}
+
+// FOR-OF
+console.log('-----------');
+let sumaForOf = 0;
+let i3 = 0;
+for (const pazymys of pazymiai) {
+    sumaForOf += pazymys;
+    console.log(`${i3++}) pazymys ${pazymys} [${sumaForOf}]`);
+}
+
+// FOREACH
+console.log('-----------');
+let sumaForEach = 0;
+pazymiai.forEach((pazymys, i4) => {
+    sumaForEach += pazymys;
+    console.log(`${i4}) pazymys ${pazymys} [${sumaForEach}]`);
+});
